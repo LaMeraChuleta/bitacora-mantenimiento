d4dcc8dcdad7d60d1b9e256f1a74bf1536c0c679
unknown <geoffreyytorres25@gmail.com>
2020-11-09 11:00:57 -0600

    Cambios_Rutas

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,7 @@
 <template>
        <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
+        <v-btn outlined @click="home">
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
@@ -11,7 +12,10 @@
         />
 
         Prosis
+        </v-btn>
       </div>
+      
+      
 
       <v-spacer></v-spacer>
 
@@ -32,6 +36,9 @@ export default {
 
         salir(){
             this.$router.push('/login')
+        },
+        home(){
+          this.$router.push('/')
         }
     }
 }
---
+++ src/views/Calendario.vue
@@ -0,0 +1,18 @@
+<script>
+import FormCalendario from "../components/Calendario.vue";
+import NavBar from "../components/NavBar.vue";
+
+export default {
+  components: {
+    FormCalendario,
+    NavBar
+  },
+};
+</script>
+
+<template>
+  <div>
+    <NavBar></NavBar>
+    <FormCalendario></FormCalendario>
+  </div>
+</template>
